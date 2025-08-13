a37cac8c925fd826b68efd94bb54c26a1e768d3e
iAngelGuzman <agelgzman@outlook.com>
2025-08-13 12:24:13 -0600

    Create VmAgregarFoto.cs
    View model para agregar foto

---
+++ ViewModels/VmAgregarFoto.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Fotografia.ViewModels
+{
+    public class VmAgregarFoto
+    {
+        public string? SMatricula { get; set; }
+        public string? BFotoBase64 { get; set; }
+    }
+}
